b4d6b9d7f86d3c6dfe3be41b423be4f917401c03
zhoujundong <zhoujundong@vipkid.com.cn>
2020-01-09 11:32:23 +0800

    spring-cloud-configuration

---
+++ tech_stack/spring-cloud/spring-cloud-configuration/src/main/java/xin/zero2one/Application.java
@@ -0,0 +1,13 @@
+package xin.zero2one;
+
+/**
+ * @author zhoujundong
+ * @data 9/1/2020
+ * @description
+ */
+public class Application {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
